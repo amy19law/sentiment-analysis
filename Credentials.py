fe8ca87915a44418f40a941ccc6e221492f40ddf
Amy Louise Law <57143504+amy19law@users.noreply.github.com>
2021-12-06 13:26:21 +0000

    Update Credentials.py

--- Credentials.py
+++ Credentials.py
@@ -1,5 +1,5 @@
 # My Twitter API Authentication Variables
-access_token = "1044485270-YIU7SPQmSCmZeLVbpGZi1lA77BwByIAlDWY6MG0"
-access_token_secret = "XYSDm7xhdgwVrQwF9OgnE84pKfkkVpmqdqSzRzOogCefc"
-consumer_key = "jUinccLJmurAmdKgcRiUnOvQn"
-consumer_secret = "wXPjrHmYaK6JsKCEn1CvMhsqT4BLZVsxDMoUckBMznYzTyhq7a"
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
